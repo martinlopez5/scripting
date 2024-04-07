ad190c8ae6efcb57ba7821ebf7ae529d1b05aeb3
martinlopez5 <martigustavolopez@gmail.com>
2024-04-07 15:22:19 -0300

    Modify script6.sh

--- week-1/lab-01/script6.sh
+++ week-1/lab-01/script6.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-touch prueba6
-echo "¡FELICITACIONES! Usted ha encontrado correctamente el archivo" >> prueba6
-read -p "Ingrese el nombre del archivo a buscar: " archivo_buscado
-
-while [ ! -f "$archivo_buscado" ]; do
-    echo "El archivo no fue encontrado, intente con otro nombre. Ej: prueba6"
-    echo "Ingrese nuevamente el nombre del archivo a buscar: "
-    read archivo_buscado
-done
-
-echo "El archivo fue encontrado correctamente, este es su contenido: "
-cat prueba6
-rm prueba6
+read -p "Ingrese el nombre del archivo a buscar: " nombre_archivo
+find $nombre_archivo
+cat $nombre_archivo
--- week-1/lab-01/script6b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "Ingrese el nombre del archivo a buscar: " nombre_archivo
-find $nombre_archivo
-cat $nombre_archivo
